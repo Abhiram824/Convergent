c918c4618c7f53c9e7f048b7c8b667757bd184b2
Rashad Philizaire <rashadphilizaire@gmail.com>
2021-10-16 13:03:13 -0500

    current changes

---
+++ reactapp/src/login/Login.js
@@ -0,0 +1,12 @@
+
+export default function Login() {
+    return (
+      <div style={{display: "flex", flex: 1}}>
+          <div style={{display: "flex", flex: 0.3, backgroundColor: "green"}}>
+          </div>
+          <div style={{display: "flex", flex: 0.7}}>
+        </div>
+      </div>
+    );
+  }
+  
---
+++ reactapp/src/recommendations/SoftwareCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function SoftwareCard(props) {
+  return (
+    <div className="Card">
+    </div>
+    // <div className="App-content">
+    //   <div className="content-subtitle">Hit next to get started.</div>
+    //   <button
+    //     className="content-button"
+    //     style={{
+    //       marginTop: 13,
+    //     }}
+    //   >
+    //   </button>
+    // </div>
+  );
+}
+
+export default SoftwareCard;
