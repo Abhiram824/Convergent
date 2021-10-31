a0086e559fa1ef6b44a94d26e7d20d7dfcef678b
rayantejani <rayantejani@utexas.edu>
2021-10-30 20:33:33 -0500

    worked on Navbar

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import SoftwareCard from "./recommendations/SoftwareCard.js";
 import Login from "./login/Login.js";
+import Navbar from "./Navbar/Navbar.js";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="App-body">
         {/* <SoftwareCard/> */}
         <Login></Login>
+
       </div>
     </div>
   );
---
+++ reactapp/src/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Recommendations',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Review',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Profile',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
