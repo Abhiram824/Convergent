6dc0755c3d86aafd4d19fb6d6e95b82aa506e8da
jatinkulkarni <71085685+jatinkulkarni@users.noreply.github.com>
2021-11-13 14:24:52 -0600

    firebase set up done

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,6 +2,23 @@ import "./App.css";
 import SoftwareCard from "./recommendations/SoftwareCard.js";
 import Login from "./login/Login.js";
 import Navbar from "./Navbar/Navbar.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMC6Eh-cAkndFN_W23MC9yA3aTl7yRa4k",
+  authDomain: "greenlit-be9a8.firebaseapp.com",
+  projectId: "greenlit-be9a8",
+  storageBucket: "greenlit-be9a8.appspot.com",
+  messagingSenderId: "875696660269",
+  appId: "1:875696660269:web:fa8e970aa5ab2e25087da4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
